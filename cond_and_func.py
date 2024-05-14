0c25f51297466ad17243a7593549cd2032d970cf
Douper <106928620+Douper2@users.noreply.github.com>
2024-05-14 14:45:58 +0300

    Add files via upload

---
+++ cond_and_func.py
@@ -0,0 +1,8 @@
+def name():
+    input("What is your name? Also capitalize the first letter of the name")
+
+name()
+if name == "Alexandru" or "Mihai":
+    print("Nice name I guess...")
+else:
+    print("Nice name...")
---
+++ functions.py
@@ -0,0 +1,12 @@
+a = '"Noi zilnic așteptăm să intrați la noi să vă salutăm.'
+b = ' Să mâncați și să ne jucam, împreună să ne distrăm"'
+
+def test_function():
+    print('"Noi zilnic așteptăm să intrați la noi să vă salutăm" - FNAF 1 song in romanian')
+
+test_function()
+
+def fnaf_song():
+    print(a + b)
+
+fnaf_song()
