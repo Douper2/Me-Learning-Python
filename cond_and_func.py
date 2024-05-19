05b86c12e4e62261d213240efed10c8a7a38126b
Douper <106928620+Douper2@users.noreply.github.com>
2024-05-19 19:32:36 +0300

    Delete cond_and_func.py

--- cond_and_func.py
+++
@@ -1,8 +0,0 @@
-def name():
-    input("What is your name? Also capitalize the first letter of the name")
-
-name()
-if name == "Alexandru" or "Mihai":
-    print("Nice name I guess...")
-else:
-    print("Nice name...")
